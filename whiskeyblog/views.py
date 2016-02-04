4763efef10f8d0ddf51fccca72692ccffe6e801b
Gian Generoso <ggeneroso@theorchard.com>
2016-02-04 17:26:30 -0500

    Prevent timestamp and author update after editing post

--- whiskeyblog/views.py
+++ whiskeyblog/views.py
@@ -34,8 +34,8 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
+            # post.author = request.user
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
