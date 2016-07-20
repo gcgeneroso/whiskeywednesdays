070f068171b8776bad5187bda11db1d81819e85b
Gian Generoso <ggeneroso@theorchard.com>
2016-07-20 15:14:28 -0400

    Add pagination

--- whiskeyblog/views.py
+++ whiskeyblog/views.py
@@ -4,9 +4,19 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    paginator = Paginator(post_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'whiskeyblog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
